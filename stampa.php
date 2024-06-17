bd62530ae4ce4d64b0fe27bccf23a4fd57c81abe
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-06-17 17:15:16 +0200

    [UPD] Importo bootstrap

--- stampa.php
+++ stampa.php
@@ -20,6 +20,8 @@ $testo_censurato_len = strlen($testo_censurato);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+
 </head>
 <body>
     <h1>
