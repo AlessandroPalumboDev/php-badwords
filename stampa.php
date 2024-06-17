49a3808c882433107471478d545144659eb1d6b0
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-06-17 16:56:19 +0200

    [FIX] Seconda stampa

--- stampa.php
+++ stampa.php
@@ -3,15 +3,15 @@
 $parola = $_GET ['parola'];
 
 $testo = $_GET ['testo'];
-$testo_len = strlen($testo)
+$testo_len = strlen($testo);
 
 $testo_censurato = str_replace($parola, '***', $testo);
-$testo_censurato_len = strlen($testo_censurato)
+$testo_censurato_len = strlen($testo_censurato);
 
 echo 'Questo è il testo da censurare:' . ' ' . $testo . '. ';
-echo 'Questa è la lunghezza del testo da censurare' . $testo_len '. ';
-echo 'Questo è il testo censurato:' . ' ' . $testo_censurato '. ';
-echo 'Questa è la lunghezza del testo censurato' . $testo_len '. ';
+echo 'Questa è la lunghezza del testo da censurare' . $testo_len . '. ';
+echo 'Questo è il testo censurato:' . ' ' . $testo_censurato . '. ';
+echo 'Questa è la lunghezza del testo censurato' . $testo_censurato_len . '. ';
 
 
 ?>
